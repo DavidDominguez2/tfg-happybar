a9d5e9f860060e5913675375520be1254e3ba0d8
David <david.dominguez.blanco.2001@gmail.com>
2022-02-17 10:36:01 +0100

    Splash

--- app/src/main/java/com/example/happybar/LoginActivity.java
+++ app/src/main/java/com/example/happybar/LoginActivity.java
@@ -2,22 +2,30 @@ package com.example.happybar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.happybar.databinding.ActivityIndexMapsBinding;
 import com.example.happybar.databinding.ActivityLoginBinding;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends Activity {
 
-    private ActivityLoginBinding binding;
     private FirebaseAuth mAuth;
+
+    private Button btnLogin;
+    private TextInputEditText inputNombre;
+    private TextInputEditText inputPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //mAuth = FirebaseAuth.getInstance();
+
+        setContentView(R.layout.activity_login);
+
+        //Button boton = findViewById(R.id.btnLogin).setBackground(R.drawable.bordes_input);
     }
 }
