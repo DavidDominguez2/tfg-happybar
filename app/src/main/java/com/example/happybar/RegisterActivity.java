b5699c8a18ff057e4b0d93e0bef28482d99e9de2
“Jaime <jaimedoguez@gmail.com>
2022-03-01 11:05:41 +0100

    registro1.0

--- app/src/main/java/com/example/happybar/RegisterActivity.java
+++ app/src/main/java/com/example/happybar/RegisterActivity.java
@@ -1,45 +1,105 @@
 package com.example.happybar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.core.Tag;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextView avisoLegal, politicaPrivacidad, condicionesGenerales;
+    private TextView avisoLegal, politicaPrivacidad, condicionesGenerales;
+    private TextInputEditText inputNombre, inputApellidos, inputCorreo, inputPwd;
+    private Button btnRegistrar;
+    private FirebaseAuth auth;
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        avisoLegal = findViewById(R.id.avisoLegal);
-        politicaPrivacidad = findViewById(R.id.politicaPrivacidad);
-        condicionesGenerales = findViewById(R.id.condicionesGenerales);
+        inputNombre = findViewById(R.id.nombreInput);
 
-        avisoLegal.setOnClickListener(new View.OnClickListener() {
+        inputApellidos = findViewById(R.id.apellidosInput);
+        inputCorreo = findViewById(R.id.correoInput);
+        inputPwd = findViewById(R.id.pwdInput);
+
+
+        auth = FirebaseAuth.getInstance();
+
+
+        btnRegistrar = findViewById(R.id.Confirmar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
-            }
-        });
-        politicaPrivacidad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
-            }
-        });
-       condicionesGenerales.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
-            }
-        });
+                auth.createUserWithEmailAndPassword(inputCorreo.toString(), inputPwd.toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+
+                        }
+
+                    }
+
+                    public void updateUI(FirebaseUser user) {
+
+                        if (user != null) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "You Signed In successfully", Toast.LENGTH_LONG);
+                            toast.show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "You Didnt signed in", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
 
 
+//AVISO LEGAL, POLITICAS y CONDICIONES
+                avisoLegal = findViewById(R.id.avisoLegal);
+                politicaPrivacidad = findViewById(R.id.politicaPrivacidad);
+                condicionesGenerales = findViewById(R.id.condicionesGenerales);
 
-    }
-}
+                avisoLegal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
+                    }
+                });
+                politicaPrivacidad.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
+                    }
+                });
+                condicionesGenerales.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(getApplicationContext(), CreditosActivity.class));
+                    }
+                });
+
+
+            }
+
+
+        });
+
+    }}
