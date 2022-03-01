a5210baa2f00c434dab481da7ec7db21d5f6cad5
= <=>
2022-03-01 18:03:41 +0100

    editar datos usuario terminado

--- app/src/main/java/com/example/happybar/AjustesActivity.java
+++ app/src/main/java/com/example/happybar/AjustesActivity.java
@@ -6,14 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.happybar.DAO.Usuario;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AjustesActivity extends AppCompatActivity {
 
     private BottomNavigationView bmenu;
     private FirebaseAuth auth;
+    private String user;
+
+    private FirebaseDatabase bbdd;
+    private DatabaseReference reference;
+
+    private TextInputEditText nombreInput, apellidosInput, correoInput;
+    private Button btnContrasena2;
+    private Usuario usu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,49 @@ public class AjustesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ajustes);
 
         auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser().getUid();
+
+
+        nombreInput = findViewById(R.id.nombreInput);
+        apellidosInput = findViewById(R.id.apellidosInput);
+        correoInput = findViewById(R.id.correoInput);
+        correoInput.setEnabled(false);
+        btnContrasena2 = findViewById(R.id.btnContrasena2);
+
+        //INSTANCIAR LA BBDD
+        bbdd = FirebaseDatabase.getInstance("https://happybar-tfg-default-rtdb.europe-west1.firebasedatabase.app/");
+        reference = bbdd.getReference().child("Usuario");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot hijo : snapshot.getChildren()) {
+
+                    if (hijo.getKey().equalsIgnoreCase(user)) {
+                        usu = hijo.getValue(Usuario.class);
+                        nombreInput.setText(usu.getNombre());
+                        apellidosInput.setText(usu.getApellido());
+                        correoInput.setText(usu.getCorreo());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        btnContrasena2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usu.setNombre(nombreInput.getText().toString());
+                usu.setApellido(apellidosInput.getText().toString());
+                reference.child(user).setValue(usu);
+            }
+        });
 
         bmenu = findViewById(R.id.bottom_navigation);
 
